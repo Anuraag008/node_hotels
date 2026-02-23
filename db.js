74fae33a76410779793f455cb80def9e6354a93e
anuraag <anuraagkumar7678@gmail.com>
2026-02-23 19:26:42 +0530

    some changes

--- db.js
+++ db.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-//const mongoURL = "mongodb://localhost:27017/hotels";
-//const mongoURL ="mongodb+srv://helloworld:qwerty12345@cluster0.ztqdi7e.mongodb.net/myDB?retryWrites=true&w=majority&authSource=admin";
+//const mongoURL = process.env.MONGODB_URL_LOCAL;
 const mongoURL = process.env.MONGODB_URL;
 mongoose.connect(mongoURL);
 
